dc18ba9443f5c7fe70218682b94ee75782b294dd
Your Name <fisayoakande@gmail.com>
2016-12-01 09:43:52 -0800

    step definitions

---
+++ features/step_definitions/taxi_req.rb
@@ -0,0 +1,56 @@
+#Request for Taxi
+
+Given(/^that I click on the "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the form to request for taxi$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the following fields: "([^"]*)", "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+#Read location from GPS
+Given(/^I am on the "([^"]*)" form$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I can see the "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+#Validate Request information
+Given(/^my GPS location is available$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^i click on the "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^my "([^"]*)" should be Liivi (\d+)$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+#Send Request for Taxi
+Given(/^I do not fill the following fields: "([^"]*)", "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on request$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should get an error to fill the required fields$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I fill "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should receive a notification that my request is being processed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
